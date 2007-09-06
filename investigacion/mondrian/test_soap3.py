fe59c2bf99acd0e2df9ef7b02785594f74c1be14
nicolas.cesar <nicolas.cesar@e72a8cf0-d837-0410-ac47-231b8028180c>
2007-09-06 22:41:22 +0000

    Problemas con SOAPpy y mondrian, sacados del wiki

---
+++ investigacion/mondrian/test_soap3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from SOAPpy import SOAPProxy
+from SOAPpy import Types
+
+url = 'http://localhost:8180/mondrian/xmla'
+n= 'urn:schemas-microsoft-com:xml-analysis'
+server = SOAPProxy(url,n)
+
+# if you want to see the SOAP message exchanged
+# uncomment the two following lines
+
+server.config.dumpSOAPOut = 1
+#server.config.dumpSOAPIn = 1
+
+server.Discover(RequestType=Types.untypedType('MDSCHEMA_CUBES'))
+
+#####
+### Genera la peticion
+# <?xml version="1.0" encoding="UTF-8"?>
+# <SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+# <SOAP-ENV:Body>
+# <ns1:Discover xmlns:ns1="urn:schemas-microsoft-com:xml-analysis" SOAP-ENC:root="1">
+# <RequestType>MDSCHEMA_CUBES</RequestType>
+# </ns1:Discover>
+# </SOAP-ENV:Body>
+# </SOAP-ENV:Envelope>
