5d4ba5a26b3de8d218d2cd2f37e5693951d53724
nicolas.cesar <nicolas.cesar@e72a8cf0-d837-0410-ac47-231b8028180c>
2007-09-17 13:54:31 +0000

    limpieza de impresion

--- lab/etl/testigos/limpiar_impresion.sh
+++ lab/etl/testigos/limpiar_impresion.sh
@@ -5,4 +5,25 @@ then
 	echo "usage: $0 archivo_impresion_octosis.txt"
 fi
 
-perl -i -p -e 'undef $/; s@\f\r\x12\r\n.*?Saldo Inicial al.*?\]@@msg' $1
+#sacamos el encabezado
+#desde:
+# el principio
+#hasta:
+#C?digo..
+perl -i -p -e 'undef $/; s@^.*?C\xa2digo:@ C\xa2digo:@s' $1
+
+
+#sacamos el encabezado por cada pagina
+#desde:
+# 0c 0d  12 0d 0a
+#hasta: 
+#Saldo Inicial al..
+perl -i -p -e 'undef $/; s@\x0c\x0d\x12\x0d\x0a.*?Saldo Inicial al.*?\]@@msg' $1
+
+
+#sacamos el pie de informe
+#desde:
+# "[saldo Final.."
+#hasta:
+# -------...-------\x0c\x0
+perl -i -p -e 'undef $/; s@\[ Saldo Final.*?-+\x0c\x0d@@s' $1
