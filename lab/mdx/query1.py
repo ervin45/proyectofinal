4ff03bae7e5278093757092e749cbed600f71d1e
nicolas.cesar <nicolas.cesar@e72a8cf0-d837-0410-ac47-231b8028180c>
2007-09-20 18:27:23 +0000

    query1 hecho en python

---
+++ lab/mdx/query1.py
@@ -0,0 +1,30 @@
+import urllib
+import urllib2
+
+url = 'http://localhost:8180/mondrian/xmla'
+headers = { 'Content-Type' : 'text/xml' }
+ 
+data = """<?xml version="1.0" encoding="UTF-8"?>
+<SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+<SOAP-ENV:Body>
+  <Execute xmlns="urn:schemas-microsoft-com:xml-analysis">
+   <Command>
+    <Statement>SELECT Measures.MEMBERS ON COLUMNS FROM Sales</Statement>
+   </Command>
+   <Properties>
+    <PropertyList>
+    <DataSourceInfo>Provider=Mondrian;DataSource=MondrianFoodMart;</DataSourceInfo>
+     <Catalog>FoodMart</Catalog>
+     <Format>Multidimensional</Format>
+     <AxisFormat>TupleFormat</AxisFormat>
+    </PropertyList>
+   </Properties>
+  </Execute>
+</SOAP-ENV:Body>
+</SOAP-ENV:Envelope>
+"""
+req = urllib2.Request(url, data, headers)
+response = urllib2.urlopen(req)
+the_page = response.read()
+
+print the_page
