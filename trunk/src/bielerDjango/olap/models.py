b9d0eabe018aee47ca5886f0f842ca166df2f276
chinomng <chinomng@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-02-06 23:23:09 +0000

    agrugue operacion PIVOT

--- trunk/src/bielerDjango/olap/models.py
+++ trunk/src/bielerDjango/olap/models.py
@@ -1,6 +1,9 @@
 import psycopg2
 import cubiculo
-              
+from pprint import pprint
+
+class Cubo:
+    pass              
                          
 class Informe:
                              
@@ -10,17 +13,70 @@ class Informe:
     def informe(self, ft, dimensions, measures):
         con_dwh = psycopg2.connect(host="127.0.0.1", port=5432, user="ncesar", password=".,supermo", database="bieler_dw")
         cursor_dwh = con_dwh.cursor()
-        c = cubiculo.Cubiculo(ft, dimensions, measures)
-        sql = c.sql()
-        cursor_dwh.execute (sql)
-        table = cursor_dwh.fetchall()
         
-        rtn = {}
+        self.cubiculo = cubiculo.Cubiculo(ft, dimensions, measures)
+        sql = self.cubiculo.sql()
+        cursor_dwh.execute(sql)
+        table = cursor_dwh.fetchall()
+        rtn = {}    
         
         for row in table:
             if not rtn.has_key(row[0]):
                 rtn[row[0]] = {}
             rtn[row[0]][row[1]] = row[2]
- 
-        return rtn                 
-                         
+            
+        ultimo = max([int(a) for a in rtn.keys()])
+        codigos = set(rtn[ultimo].keys())
+        valores = {}
+        
+        for anio in rtn.values():
+            for codigo in codigos:
+                if not valores.get(codigo, False):
+                    valores[codigo] = []
+                valores[codigo].append(anio.get(codigo, 0))            
+                
+        cubo = Cubo()
+        cubo.header = rtn.keys()
+        cubo.body = valores
+        
+        return cubo 
+    
+    def pivot(self):
+        con_dwh = psycopg2.connect(host="127.0.0.1", port=5432, user="ncesar", password=".,supermo", database="bieler_dw")
+        cursor_dwh = con_dwh.cursor()        
+        
+        self.cubiculo.pivot()
+        
+        sql = self.cubiculo.sql()
+        cursor_dwh.execute(sql)
+        table = cursor_dwh.fetchall()
+
+        rtn = {}    
+       
+        for row in table:
+            if not rtn.has_key(row[0]):
+                rtn[row[0]] = {}
+            rtn[row[0]][row[1]] = row[2]            
+        
+        sql_second_dimension_values = self.cubiculo.second_dimension_values()
+        cursor_dwh.execute(sql_second_dimension_values)
+   
+        codigos = cursor_dwh.fetchall()        
+        codigos = [x[0] for x in codigos]
+        
+        valores = {}
+       
+        for anio in rtn.values():
+            for codigo in codigos:
+                if not valores.get(codigo, False):
+                    valores[codigo] = []
+                valores[codigo].append(anio.get(codigo, 0))            
+               
+        cubo = Cubo()
+        cubo.header = rtn.keys()
+        cubo.body = valores
+       
+        return cubo               
+        
+               
+    
