4365f8d19b7c0dc4ddfa324ff738e1a1f5c0b368
chinomng <chinomng@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-02-07 23:10:40 +0000

    empeze el drill_replacing

--- src/bielerDjango/urls.py
+++ src/bielerDjango/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     (r'^reportes/$', reportes),
     (r'^pivot/$', pivot),
     (r'^drill/(.*)/$', drill),
+    (r'^drill_replacing/(.*)/(.*)/$', drill_replacing),
     (r'^roll/(.*)/$', roll),
     
     (r'^admin/', include('django.contrib.admin.urls')),
