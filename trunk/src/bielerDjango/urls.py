31108688c40dd67f63a9d5d919be6cc8e9af2c62
chinomng <chinomng@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-02-08 20:31:10 +0000

    operaciones de drill_replacing y drill_replacing2

--- trunk/src/bielerDjango/urls.py
+++ trunk/src/bielerDjango/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     (r'^pivot/$', pivot),
     (r'^drill/(.*)/$', drill),
     (r'^drill_replacing/(.*)/(.*)/$', drill_replacing),
+    (r'^drill_replacing2/(.*)/(.*)/$', drill_replacing2),
     (r'^roll/(.*)/$', roll),
     
     (r'^admin/', include('django.contrib.admin.urls')),
