6cbbb93f168119943f5be32204cccd454c88a528
nicolas.cesar <nicolas.cesar@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-02-10 23:17:38 +0000

    etl de ventas del tipo Salida x taller.

--- lab/etl/etl_ventas_taller.py
+++ lab/etl/etl_ventas_taller.py
@@ -22,9 +22,10 @@ cursor_dwh = con_dwh.cursor()
 for anio in range(1998,2008):
   for mes in range(1,13):
     # Make SQL string and execute it
-  ###
-  ### E1-1 | Compra | "1-Remito"
-  ###
+    #
+    # S1-1 | Venta por taller | 'SAL +OR: +[0-9]+ +S\.Ap\.[0-9]+'
+    # S3-  | Venta por mostrador | 'SAL x Venta \(Pv\. +[0-9\.]+\)'
+    #
     sql = """
 SELECT 
  substring(A.ART_CODIGO, 7, 2)  as Grupo_Constructivo1,
@@ -36,8 +37,9 @@ SELECT
  A.ART_DESCRI as Descripcion,
  A.ART_CODIGO as Codigo,
  SUM(M.ART_CANTID) as Cantidad,
- SUM(M.ART_PRECIO * M.ART_CANTID) as Costo_Pesos,
- SUM(IF(M.ART_PREDOL > 0, M.ART_PREDOL, M.ART_PRECIO) * M.ART_CANTID) as Costo_Dolar
+ SUM(M.ART_PREVT * M.ART_CANTID) as Precio_Vta_Pesos,
+ SUM((M.ART_PREVT - M.ART_PRECIO)* M.ART_CANTID) as Margen_Pesos,
+ 'Taller' as Tipo 
 FROM
  `STS_ARTIC0` A 
  Join `STS_MOVIM0` M on (A.ART_CODIGO = M.ART_CODIGO)
@@ -45,9 +47,8 @@ WHERE
   A.`ART_DEFCOD` = 'A'
   and YEAR(M.ART_FECHA) =  '%s'
   and MONTH(M.ART_FECHA) = '%s'
-  and M.ART_TIPMOV = 'E1'
+  and M.ART_TIPMOV = 'S1'
   and M.ART_CODSAL = ' 1'
-
 GROUP BY 
  A.ART_CODIGO
 """ % (anio,mes)
@@ -66,8 +67,9 @@ GROUP BY
          descripcion,
          codigo,
          cantidad,
-         costo_pesos,
-         costo_dolar) = row[0:11]
+         precio_vta_pesos,
+         margen_pesos,
+         tipo) = row[0:12]
 
         print anio, mes, codigo
 
@@ -75,13 +77,14 @@ GROUP BY
 
         tiempo_id = etlutils.get_id(cursor_dwh,'td_tiempo','id', {'anio':anio, 'mes':mes})
 
+        tipo_venta_id = etlutils.get_id(cursor_dwh,'td_tipo_venta','id', {'tipo_venta': tipo})
 
         pieza_id = etlutils.get_id(cursor_dwh,'td_pieza','id',{'grupo_constructivo': grupo_constructivo, 'modelo':modelo,'modificacion': modificacion, 'pieza': pieza, 'descripcion': descripcion, 'codigo': codigo})
 
         proveedor_id=etlutils.get_id(cursor_dwh,'td_proveedor','id',{'id_octosis':proovedor})
 
-        sql = 'insert into ft_compras (fk_tiempo, fk_pieza, fk_proveedor, cantidad, costo_pesos, costo_dolar) values (%s, %s, %s, %s, %s, %s)'
-        cursor_dwh.execute (sql, (tiempo_id, pieza_id, proveedor_id, cantidad, costo_pesos, costo_dolar))
+        sql = 'insert into ft_ventas (fk_tiempo, fk_pieza, fk_proveedor, fk_tipo_venta, cantidad, precio_venta_pesos, margen_pesos) values (%s, %s, %s, %s, %s, %s, %s)'
+        cursor_dwh.execute (sql, (tiempo_id, pieza_id, proveedor_id, tipo_venta_id, cantidad, precio_vta_pesos, margen_pesos))
         con_dwh.commit()
 
 
