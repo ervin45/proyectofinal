daec04a52fc183400aea9fedb81b255ab1d314bb
chinomng <chinomng@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-02-12 04:39:39 +0000

    url bookmarkeable. Todavia tengo que parsear el referer, boton atras me caga

--- trunk/src/bielerDjango/olap/views.py
+++ trunk/src/bielerDjango/olap/views.py
@@ -1,45 +1,60 @@
 import models
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from pprint import pprint
 
 def costo_promedio(costo_pesos, cantidad):
     return float(costo_pesos) / float(cantidad)
 
-def reportes(request):
-    informe = models.Informe()
-    #cube = informe.informe("movimientos", [["pieza", "modelo"], ["tiempo","anio"]], [["stock", "avg"]]) 
-    cube = informe.informe("compras", 
-                         [["pieza", "grupo_constructivo"], ["tiempo","anio"]], 
-                         [["cantidad", "sum"],['costo_pesos', 'sum']], 
-                           costo_promedio) 
-    request.session["informe"] = informe  
+def report(request,report, x, y, xl, yl, xr="", yr="", ore=""):
+    #informe = models.Informe()
+    
+    report = models.Report()
+    
+    ##VIENE DE LA BD
+    report.ft = "compras" #report
+    report.measures = [["cantidad", "sum"],['costo_pesos', 'sum']]
+    report.member_function = costo_promedio
+    ##VIENE DE LA BD
+    
+    report.x = x
+    report.xl = xl
+    report.y = y
+    report.yl = yl
+    report.xr = xr
+    report.yr = yr
+    report.ore = ore
+    
+    cube = report.get_cube()
+    
+    request.session['informe'] = report
+    print "body or"
+    print cube.header
+    print cube.body_order 
+    print cube.body
     return render_to_response('reportes.html',{'cube':cube})
 
 def pivot(request):
     a = request.session["informe"]
-    cube = a.pivot()
-    request.session["informe"] = a
-    return render_to_response('reportes.html',{'cube':cube})
+    url = a.pivot()
+    return HttpResponseRedirect(url)
     
 def roll(request, axis):
     a = request.session["informe"]
-    cube = a.roll(axis)
-    request.session["informe"] = a
-    return render_to_response('reportes.html',{'cube':cube})
+    url = a.roll(axis)
+    return HttpResponseRedirect(url)
 
 def drill(request, axis):
     a = request.session["informe"]
-    cube = a.drill(axis)
-    request.session["informe"] = a
-    return render_to_response('reportes.html',{'cube':cube})
+    url = a.drill(axis)
+    return HttpResponseRedirect(url)
 
 def drill_replacing(request, axis, value):
     a = request.session["informe"]
-    cube = a.drill_replacing(axis, value)
-    request.session["informe"] = a
-    return render_to_response('reportes.html',{'cube':cube})    
+    url = a.drill_replacing(axis, value)
+    return HttpResponseRedirect(url)
 
 def drill_replacing2(request, value0, value1):
     a = request.session["informe"]
-    cube = a.drill_replacing2(value0, value1)
-    request.session["informe"] = a
-    return render_to_response('reportes.html',{'cube':cube})    
+    url = a.drill_replacing2(value0, value1)
+    return HttpResponseRedirect(url)
