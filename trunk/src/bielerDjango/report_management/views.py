077be3bd43b92f23edbf303b9a406bc42cccdc5e
chinomng <chinomng@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-02-18 20:42:55 +0000

    agregue la aplicacion report_management

---
+++ trunk/src/bielerDjango/report_management/views.py
@@ -0,0 +1,8 @@
+import models
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from pprint import pprint
+
+def add(request):
+    pass
