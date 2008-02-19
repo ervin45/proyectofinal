7251a0cf329d3d1c30a98532225f748700aadfd5
nicolas.cesar <nicolas.cesar@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-02-19 15:24:53 +0000

    podada de codigo y nuevos campos: egreso e ingreso mensual

--- lab/etl/etl_movimientos.py
+++ lab/etl/etl_movimientos.py
@@ -19,193 +19,9 @@ cursor_dwh = con_dwh.cursor()
 
 
 #info...
-['Anio','Mes','Grupo_Constructivo1','Grupo_Constructivo2','Modelo','Modificacion','Pieza','Proovedor','Descripcion','Codigo','Stock','Costo_pesos','Precio_Vta', 'Compras', 'Devoluciones', 'Ventas', 'VentaPorTaller']
+['Anio','Mes','Grupo_Constructivo1','Grupo_Constructivo2','Modelo','Modificacion','Pieza','Proovedor','Descripcion','Codigo','Stock','Egreso', 'Ingreso']
 
 
-
-
-def get_costo_precio_etc(row,cursor):
-  anio=row[0]
-  mes=row[1]
-  codigo=row[9]
-
-
-  ###
-  ### E1-1 | Compra | "1-Remito"
-  ###
-  sql=""" 
-SELECT
- M.ART_PRECIO * M.ART_CANTID as Costo_Pesos
-FROM
- `STS_ARTIC0` A
-  Join `STS_MOVIM0` M on (A.ART_CODIGO = M.ART_CODIGO)
-WHERE
-  A.`ART_DEFCOD`   = 'A'
-  and A.ART_CODIGO ='%s'
-  and M.ART_FECHA < '%s-%s-01'
-  and M.ART_TIPMOV = 'E1'
-  and M.ART_CODSAL = '1'
-  and (M.ART_PRECIO + M.ART_PREDOL) > 0
-ORDER BY
-  M.ART_FECHA DESC LIMIT 1
-""" % (codigo,anio,mes)
-  cursor2.execute(sql)
-  costos = cursor2.fetchone()  
-
-  if not costos:
-    sql=""" 
-SELECT
- M.ART_PRECIO as Costo_Pesos
-FROM
- `STS_ARTIC0` A
-  Join `STS_MOVIM0` M on (A.ART_CODIGO = M.ART_CODIGO)
-WHERE
-  A.`ART_DEFCOD`   = 'A'
-  and A.ART_CODIGO ='%s'
-  and M.ART_FECHA < '%s-%s-01'
-  and (M.ART_PRECIO + M.ART_PREDOL) > 0
-ORDER BY
-  M.ART_FECHA DESC LIMIT 1
-""" % (codigo,anio,mes)
-    cursor2.execute(sql)
-    costos = cursor2.fetchone()  
-
-  sql="""
-SELECT
- M.ART_PREVT as Precio_Vta
-FROM
- `STS_ARTIC0` A
-  Join `STS_MOVIM0` M on (A.ART_CODIGO = M.ART_CODIGO)
-WHERE
-  A.`ART_DEFCOD`   = 'A'
-  and A.ART_CODIGO ='%s'
-  and M.ART_FECHA < '%s-%s-01'
-  and
-
-  (M.ART_TIPMOV = 'S3' or
-   (M.ART_TIPMOV = 'S3' and  and M.ART_CODSAL = '1')
-  )
-  and (M.ART_PRECIO + M.ART_PREDOL) > 0
-ORDER BY
-  M.ART_FECHA DESC LIMIT 1
-""" % (codigo,anio,mes)
-  cursor2.execute(sql)
-  venta = cursor2.fetchone()
-
-  #pudo Solo haber existido ingreso
-  if not venta:
-    sql="""
-SELECT
- M.ART_PREVT as Precio_Vta
-FROM
- `STS_ARTIC0` A
-  Join `STS_MOVIM0` M on (A.ART_CODIGO = M.ART_CODIGO)
-WHERE
-  A.`ART_DEFCOD`   = 'A'
-  and A.ART_CODIGO ='%s'
-  and M.ART_FECHA < '%s-%s-01'
-  and (M.ART_PRECIO + M.ART_PREDOL) > 0
-ORDER BY
-  M.ART_FECHA DESC LIMIT 1
-""" % (codigo,anio,mes)
-    cursor2.execute(sql)
-    venta = cursor2.fetchone()
- 
-  sql="""
-SELECT
- SUM(M.ART_CANTID) as Compras
-FROM
- `STS_ARTIC0` A
-  Join `STS_MOVIM0` M on (A.ART_CODIGO = M.ART_CODIGO)
-WHERE
-  A.`ART_DEFCOD`   = 'A'
-  and A.ART_CODIGO ='%s'
-  and YEAR(M.ART_FECHA) = '%s'
-  AND MONTH(M.ART_FECHA) = '%s'
-  and M.ART_TIPMOV = 'E1'
-ORDER BY
-  M.ART_FECHA DESC LIMIT 1
-""" % (codigo,anio,mes)
-  cursor2.execute(sql)
-  cantidad_compras = cursor2.fetchone()
-  if cantidad_compras == (None,):
-    cantidad_compras = (0.0, )
-
-  sql="""
-SELECT
- SUM(M.ART_CANTID) as Ventas
-FROM
- `STS_ARTIC0` A
-  Join `STS_MOVIM0` M on (A.ART_CODIGO = M.ART_CODIGO)
-WHERE
-  A.`ART_DEFCOD`   = 'A'
-  and A.ART_CODIGO ='%s'
-  and YEAR(M.ART_FECHA) = '%s'
-  AND MONTH(M.ART_FECHA) = '%s'
-  and M.ART_TIPMOV = 'S3'
-ORDER BY
-  M.ART_FECHA DESC LIMIT 1
-""" % (codigo,anio,mes)
-  cursor2.execute(sql)
-  cantidad_ventas = cursor2.fetchone()
-  if cantidad_ventas == (None,):
-    cantidad_ventas = (0.0, )
-
-  sql="""
-SELECT
- SUM(M.ART_CANTID) as OtroEgreso 
-FROM
- `STS_ARTIC0` A
-  Join `STS_MOVIM0` M on (A.ART_CODIGO = M.ART_CODIGO)
-WHERE
-  A.`ART_DEFCOD`   = 'A'
-  and A.ART_CODIGO ='%s'
-  and YEAR(M.ART_FECHA) = '%s'
-  AND MONTH(M.ART_FECHA) = '%s'
-  and M.ART_TIPMOV = 'S1'
-ORDER BY
-  M.ART_FECHA DESC LIMIT 1
-""" % (codigo,anio,mes)
-  cursor2.execute(sql)
-  cantidad_otroegreso = cursor2.fetchone()
-  if cantidad_otroegreso == (None,):
-    cantidad_otroegreso = (0.0, )
-
-  sql="""
-SELECT
- SUM(M.ART_CANTID) as Devoluciones
-FROM
- `STS_ARTIC0` A
-  Join `STS_MOVIM0` M on (A.ART_CODIGO = M.ART_CODIGO)
-WHERE
-  A.`ART_DEFCOD`   = 'A'
-  and A.ART_CODIGO ='%s'
-  and YEAR(M.ART_FECHA) = '%s'
-  AND MONTH(M.ART_FECHA) = '%s'
-  and M.ART_TIPMOV = 'E3'
-ORDER BY
-  M.ART_FECHA DESC LIMIT 1
-""" % (codigo,anio,mes)
-  cursor2.execute(sql)
-  cantidad_devoluciones = cursor2.fetchone()
-  if cantidad_devoluciones == (None,):
-    cantidad_devoluciones = (0.0, )
-
-  rtn=[]
-  try:
-    rtn.extend(costos)
-    rtn.extend(venta)
-    rtn.extend(cantidad_compras)
-    rtn.extend(cantidad_devoluciones)
-    rtn.extend(cantidad_ventas)
-    rtn.extend(cantidad_otroegreso)
-  except:
-    print codigo
-    print costos
-    raise
-
-  print rtn
-  return rtn
 
 for anio in range(1998,2008):
   for mes in range(1,13):
@@ -221,6 +37,8 @@ SELECT
  A.ART_DESCRI as Descripcion,
  A.ART_CODIGO as Codigo,
  SUM( IF( M.ART_TIPMOV LIKE 'S%%', - M.ART_CANTID, M.ART_CANTID ) ) + AP.ART_SDOINI AS Stock
+ SUM( IF( M.ART_TIPMOV LIKE 'S%%' AND YEAR(M.ART_FECHA) = '%s' AND MONTH(M.ART_FECHA) = '%s', M.ART_CANTID, 0 ) ) as Egresos,
+ SUM( IF( M.ART_TIPMOV NOT LIKE 'S%%' AND YEAR(M.ART_FECHA) = '%s' AND MONTH(M.ART_FECHA) = '%s', M.ART_CANTID, 0 ) ) as Ingresos
 FROM
  `STS_ARTIC0` A 
  Join `STS_MOVIM0` M on (A.ART_CODIGO = M.ART_CODIGO)
@@ -232,15 +50,12 @@ WHERE
   and M.ART_FECHA < '%s-%s-01'
 GROUP BY 
  A.ART_CODIGO
-""" % (anio,mes)
+""" % (anio,mes,anio,mes,anio,mes)
     cursor.execute(sql)
 
     # Fetch all results from the cursor into a sequence and close the connection
     results = cursor.fetchall()
     for row in results:
-	#someiterable = [anio, mes]
-   	#someiterable.extend(row)
-   	#someiterable.extend(get_costo_precio_etc(someiterable,cursor2))
 
     	(grupoConstructivo1, 
          grupoConstructivo2,
@@ -250,7 +65,9 @@ GROUP BY
          proovedor,
          descripcion,
          codigo,
-         stock) = row[0:9]
+         stock,
+         egresos,
+         ingresos) = row[0:11]
 
         grupo_constructivo = grupoConstructivo1 + grupoConstructivo2
         
@@ -261,8 +78,8 @@ GROUP BY
 
         proveedor_id=etlutils.get_id(cursor_dwh,'td_proveedor','id',{'id_octosis':proovedor})
 
-        sql = 'insert into ft_movimientos (fk_tiempo, fk_pieza, fk_proveedor, stock) values (%s, %s, %s, %s)'
-        cursor_dwh.execute (sql, (tiempo_id, pieza_id, proveedor_id, stock))
+        sql = 'insert into ft_movimientos (fk_tiempo, fk_pieza, fk_proveedor, stock, egresos, ingresos) values (%s, %s, %s, %s, %s, %s)'
+        cursor_dwh.execute (sql, (tiempo_id, pieza_id, proveedor_id, stock, egresos, ingresos))
         con_dwh.commit()
 
 
