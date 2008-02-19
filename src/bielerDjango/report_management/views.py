d9adbe6b68116f6e604ff8e3d29d5d0559236a9b
chinomng <chinomng@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-02-19 04:30:36 +0000

    primera aprox a la gestion de reportes

--- src/bielerDjango/report_management/views.py
+++ src/bielerDjango/report_management/views.py
@@ -1,8 +1,20 @@
-import models
+from models import ReportTemplate
+
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from pprint import pprint
 
-def add(request):
-    pass
+def report_list(request):
+    reports = ReportTemplate.objects.all()
+    
+    return render_to_response('report_list.html',locals())
+
+def insert(request):
+    return render_to_response('insert.html',locals())
+
+def delete(request, id):
+    report = ReportTemplate.objects.get(id=id)
+    report.delete()
+    
+    return HttpResponseRedirect("/report/report_list/")
