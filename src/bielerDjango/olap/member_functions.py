d13e2ba53e4e2e9bfa03725dfb7f651a6b6c0cb3
chinomng <chinomng@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-02-25 22:26:26 +0000

    agregue archivo con las member function predefinidas

---
+++ src/bielerDjango/olap/member_functions.py
@@ -0,0 +1,15 @@
+
+def dividir(arg0, arg1):
+    return arg[0] / arg[1]
+
+def sumar(arg0, arg1):
+    if not arg0:
+        arg0 = 0
+
+    if not arg1:
+        arg1 = 0
+
+    return arg0 + arg1
+
+def same(arg):
+    return arg
