7677036ce4a1372a30e8696e6becf57c6bb6e97d
nicolas.cesar <nicolas.cesar@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-03-04 22:21:52 +0000

    cambios en correccion4, para los precios de venta en dolares y margenes

--- sql/correccion4.py
+++ sql/correccion4.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 ## corrige STS_MOVIM0.M_PRECIOVTA y pone en reemplazo el "costo" basandose
 ## en la ultima venta, caso contrario los deja tal cual.
-## si encuentra cotización para ese dia se calcula el 
+## si encuentra cotizacion para ese dia se calcula el 
 
 
 import datetime
@@ -23,6 +23,7 @@ def dbg(s, level = 10):
 
 sql = '''SELECT
   M.ART_FECHA as Fecha,
+  M.ART_HORA as Hora,
   M.ART_CODIGO as Codigo,
   M.ART_PREDOL as Predol,
   M.ART_PRECIO as Precio
@@ -30,16 +31,20 @@ FROM
   `STS_MOVIM0` M
 WHERE
   M.`ART_DEFCOD` = 'A'
-  and M.ART_TIPMOV = 'S1'
-  and M.ART_CODSAL = ' 1'
+  and M.ART_TIPMOV = 'S3'
 
 '''
+  #and M.ART_TIPMOV = 'S1'
+  #and M.ART_CODSAL = ' 1'
+  ## Y 'S3'???
+  ## select X_PRECIO_PESOS from STS_MOVIM0 where art_codigo = ' 009060380171000000000' and ART_TIPMOV = 'S3';
+  ## me esta devolviendo todo NULL!
 cursor.execute(sql)
 
 dbg("Cantidad de registros a procesar: %d" % cursor.rowcount, 3)
 for row in cursor.fetchall():
     start = time.time()
-    (fecha, codigo, predol, precio) = row
+    (fecha, hora, codigo, predol, precio) = row
     #print "procesing...", row
     dbg("01 - %f" % (time.time() - start ))
     sql_costo = '''SELECT
@@ -64,11 +69,11 @@ LIMIT 0,1
     dbg("02 - %f" % (time.time() - start ))
     cursor2.execute(sql_costo)
     dbg("03 - %f" % (time.time() - start ))       
-    costos = cursor.fetchall()  
+    costos = cursor2.fetchall()  
     dbg("04 - %f" % (time.time() - start ))       
   
     if len(costos) == 1:
-        precio_historico_pesos = row[0]
+        precio_historico_pesos = costos[0][2]
     else:
         precio_historico_pesos = precio
 
@@ -79,14 +84,17 @@ LIMIT 0,1
         dbg("04.01 - %f" % (time.time() - start ))       
         cursor3.execute(sql_cotizacion)
         dbg("04.02 - %f" % (time.time() - start ))       
-        cotizacion = cursor.fetchone()
+        cotizacion = cursor3.fetchall()
         dbg("04.03 - %f" % (time.time() - start ))       
-        if cotizacion == None:
-            #print "warning: no existe cotizacion para fecha %s" % fecha
+        if not cotizacion:
+            dbg( "warning: no existe cotizacion para fecha %s" % fecha, 2)
             precio_historico_dolares = precio
+            dbg( "setting precio_historico_dolares = %s" % precio_historico_dolares,12)
         else:
-            precio_historico_dolares = predol / cotizacion
+            precio_historico_dolares = precio / cotizacion[0][0]
+            
+            dbg( "setting precio_historico_dolares = %s, precio = %s and cotizacion[0][0] = %s" % (precio_historico_dolares,precio, cotizacion[0][0]) ,12)
 
-    print """UPDATE `STS_MOVIM0` set ART_PRECIO='%s', ART_PREDOL='%s'  WHERE ART_CODIGO = '%s' and ART_FECHA = '%s';""" % (precio_historico_pesos, precio_historico_dolares, codigo, fecha)
+    print """UPDATE `STS_MOVIM0` set X_PRECIO_PESOS='%s', X_PRECIO_DOLARES='%s' WHERE ART_DEFCOD = 'A' AND ART_CODIGO = '%s' and ART_FECHA = '%s' and ART_HORA = '%s';""" % (precio_historico_pesos, precio_historico_dolares, codigo, fecha, hora)
     dbg("** - %f" % (time.time() - start ), 3)
 
