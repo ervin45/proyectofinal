c81605863d8e622652506d8874ae0e14d2611be5
chinomng <chinomng@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-03-04 22:59:05 +0000

    agregue utils.py

---
+++ src/bielerDjango/olap/utils.py
@@ -0,0 +1,22 @@
+def isFloat(s):
+    try:
+        return float(s) or True
+    except:
+        return False
+
+def compare(a,b):
+    primero = str(a).split(' - ')
+    segundo = str(b).split(' - ')
+    for x,y in zip(primero, segundo):
+        if isFloat(x) and isFloat(y):
+            if float(x) > float(y):
+                return 1
+            elif float(x) < float(y):
+                return -1
+        else:
+            if x > y:
+                return 1
+            elif x < y:
+                return -1
+    print "returning 0"
+    return 0
