c22f19b78bcf60c459c61b9c499a332466fd07bd
nicolas.cesar <nicolas.cesar@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-03-05 21:04:49 +0000

    DeprecationWarning soved

--- src/bielerDjango/report_management/models.py
+++ src/bielerDjango/report_management/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class ReportTemplate(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(maxlength=60)
-    ft = models.CharField(maxlength=60)
-    default_dwp = models.CharField(maxlength=600)
+    name = models.CharField(max_length=60)
+    ft = models.CharField(max_length=60)
+    default_dwp = models.CharField(max_length=600)
     user_id = models.IntegerField()
     
     class Admin:
