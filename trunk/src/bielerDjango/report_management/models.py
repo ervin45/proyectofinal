7b846894acae425f0e70fec59675a4fa112b0876
nicolas.cesar <nicolas.cesar@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-03-06 14:43:18 +0000

    incompatible django versions... deleted unused class

--- trunk/src/bielerDjango/report_management/models.py
+++ trunk/src/bielerDjango/report_management/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 
-class ReportTemplate(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField()
-    ft = models.CharField()
-    default_dwp = models.CharField()
-    user_id = models.IntegerField()
-    
-    class Admin:
-        pass
-    
-    class Meta:
-        db_table = 'report_template'
 
