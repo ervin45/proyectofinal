f71e124968cba6825a99aeea3fab3216452e42e9
nicolas.cesar <nicolas.cesar@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-03-16 22:09:24 +0000

    material ReST para entregar a la catedra

---
+++ doc/material_para_la_catedra/make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rst2latex --language=es \
+          --no-doc-title \
+	  --documentclass=book \
+	  --documentoptions=12pt,a4paper,spanish \
+	  --section-numbering \
+	  --file-insertion-enabled \
+	  --use-latex-toc  < documentacion_final.txt > documentacion_final.tex
+	  
+latex documentacion_final.tex
