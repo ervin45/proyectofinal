85ff8631406aae465160b153bffe50bf8e1af33a
nicolas.cesar <nicolas.cesar@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-03-16 22:50:53 +0000

    Las recomendaciones de forma fueron aisladas en un stylesheet de LaTeX

--- doc/material_para_la_catedra/make.sh
+++ doc/material_para_la_catedra/make.sh
@@ -4,6 +4,7 @@ rst2latex --language=es \
           --no-doc-title \
 	  --documentclass=book \
 	  --documentoptions=12pt,a4paper,spanish \
+	  --stylesheet=proyecto.stx \
 	  --section-numbering \
 	  --file-insertion-enabled \
 	  --use-latex-toc  < documentacion_final.txt > documentacion_final.tex
