57379c58bf2ca30223a1e82d95bbffebfaa10abb
chinomng <chinomng@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-05-01 22:51:16 +0000

    olvide agregar dw_connect.py

---
+++ src/bielerDjango/olap/dw_connect.py
@@ -0,0 +1,7 @@
+DATABASE_HOST = '192.168.61.102'
+
+def cursor():
+    import psycopg2
+    import psycopg2.extras
+    con = psycopg2.connect(host=DATABASE_HOST, port=5432, user="ncesar", password=".,supermo", database="bieler_dw")
+    return con.cursor(cursor_factory=psycopg2.extras.DictCursor) 
