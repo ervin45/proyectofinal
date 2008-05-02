14b62f8056754f1962210d4c980bc2b82df1b771
chinomng <chinomng@e72a8cf0-d837-0410-ac47-231b8028180c>
2008-05-02 00:02:31 +0000

    wrap al scrip de doctest

--- src/bielerDjango/generate_doc_test.py
+++ src/bielerDjango/generate_doc_test.py
@@ -3,6 +3,21 @@
 from olap import cubiculo
 from olap import reports
 
+def wrap(text, width):
+    """
+    A word-wrap function that preserves existing line breaks
+    and most spaces in the text. Expects that existing line
+    breaks are posix newlines (\n).
+    """
+    return reduce(lambda line, word, width=width: '%s%s%s' %
+                  (line,
+                   ' \n'[(len(line)-line.rfind('\n')-1
+                         + len(word.split('\n',1)[0]
+                              ) >= width)],
+                   word),
+                  text.split(' ')
+                 )
+
 instance_type = type(cubiculo.Cubiculo._select), type(cubiculo.Meta.previous) 
 
 classes = [(cubiculo.Cubiculo, "cubiculo.Cubiculo"), (cubiculo.Meta, "cubiculo.Meta"), (reports.Cube, "reports.Cube"), (reports.Report1, "reports.Report1"), (reports.Report2, "reports.Report2"), (reports.Ajax_responser, "reports.Ajax_responser")]
@@ -20,4 +35,4 @@ for c in classes:
             if eval_doc != None:
 
                 print 'Metodo: %s' % eval_name
-                print '%s' % eval_doc
+                print "\n".join(["          %s" % x.strip() for x in wrap(eval_doc, 40).split("\n")])
